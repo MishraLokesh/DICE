1405c239d218a0d749aef000d2af51200832056d
Lokesh Mishra <4man.mishra@gmail.com>
2021-08-18 00:45:10 +0530

    created appropriate dir

--- Contracts/DiceGame.sol
+++ Contracts/DiceGame.sol
@@ -1,13 +1,14 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.4;
 
 contract Dice{
 
 	struct Bet{
 		uint8  currentBet;
 		bool  isBetSet; //default value is false	
-		uint8  destiny;
-        bool matchFound;
-        uint count;
+		uint  destiny;
+    bool matchFound;
+    uint count;
 	}
 
 	mapping(address => Bet) private bets;
@@ -18,9 +19,6 @@ contract Dice{
 
 	event GameResult(address bidder, uint8 currentBet , uint8 destiny);
 
-	constructor() public{
-		randomFactor = 10;
-	}
 
 	function isBetSet() public view returns(bool){
 		return bets[msg.sender].isBetSet;
@@ -34,7 +32,6 @@ contract Dice{
 	function wonORnot() public view returns(bool){
 		return bets[msg.sender].matchFound;
 	}
-
 	function getNewbet(uint8 a) public {
 		require(bets[msg.sender].isBetSet == false);
 		bets[msg.sender].isBetSet = true;
@@ -46,10 +43,10 @@ contract Dice{
 		bets[msg.sender].count++;
         require(bets[msg.sender].isBetSet == true);
 
-        bets[msg.sender].destiny = (uint8(keccak256(abi.encodePacked(block.difficulty, now, bets[msg.sender].count))) % 5) + 1;
+        bets[msg.sender].destiny = (uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, bets[msg.sender].count))) % 5) + 1;
     
 		if(bets[msg.sender].destiny == bets[msg.sender].currentBet){
-			msg.sender.transfer(100000000000000);
+			// msg.sender.transfer(100000000000000);
 			bets[msg.sender].matchFound = true;
 		}
 		if(bets[msg.sender].count == 3)
